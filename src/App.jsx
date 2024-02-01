f3c8643100524428e9044c551ca0ff294896c729
bee-gg <122276079+bee-gg@users.noreply.github.com>
2024-02-01 00:52:07 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,57 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./components/winning-combinations";
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+    if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+      currentPlayer = "O";
+    }
+    return currentPlayer;
+}
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]); //turn data
-  const [activePlayer, setActivePlayer] = useState('X');
+  //const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
-    setGameTurns(prevTurns => { 
-      let currentPlayer = 'X';
-      if (prevTurns[0].player === 'X');
+    //setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
-      const updatedTurns = [{ square: {row: rowIndex, col: colIndex}, player: activePlayer}, ...prevTurns
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer }, //player is the symbol
+        ...prevTurns,
       ];
+      return updatedTurns;
     });
   }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
-        <Log />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        />
       </div>
+        <Log turns={gameTurns}/>
     </main>
   );
-} 
+}
 
 export default App;
- 
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,30 @@
-//import { useState } from "react";
 
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+
+  for (const turn of turns){ // if turns is empty array - loop wont execute
+    const { square, player } = turn; // destructuring from turns array v bellow too
+    const { row, col} = square;
+
+    gameBoard[row][col] = player; // deriving state - gameTurns from App
+  }  
+
+
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   // function handleSelectSquare(rowIndex, colIndex){ //to find which null to click
   //   setGameBoard((prevGameBoard) => {
   //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; //brand ndew array full of new nested arrays which stores data they stored before
   //     //'...prevGameBoard' new array object in memory that contains the old array elements as child elements
-      
+
   //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
   //     return updatedBoard;
-  //   });
+  //   }); 
 
   //   onSelectSquare();
   // }
@@ -26,7 +35,11 @@ export default function GameBoard({ onSelectSquare }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
